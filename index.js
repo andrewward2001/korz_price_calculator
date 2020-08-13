e5dbfa1c71e67bdbe93c598fde605f6c137dc57f
Andrew Ward <tmiandreww@gmail.com>
2020-08-12 22:28:44 -0400

    rest of selection animations

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 $(document).ready(() => {
     $('#premium-select').on('click', (e) => {
         $('#premium-card').addClass('selected')
-        console.log(parseInt($('#basic-card').css('width')) + 48)
 
         $('#basic-card').css({position: 'relative', 'z-index': -1})
         $('#basic-card').animate({
-            left: parseInt($('#basic-card').css('width')) + 48
-        }, 500, () => {
-            $('#basic-card').css({opacity: 0})
-        })
+            left: parseInt($('#basic-card').css('width')) + 48,
+            opacity: 0
+        }, 500)
 
+
+        $('#deluxe-card').css({position: 'relative', 'z-index': -1})
+        $('#deluxe-card').animate({
+            right: parseInt($('#deluxe-card').css('width')) + 48,
+            opacity: 0
+        }, 500)
+    })
+
+    $('#basic-select').on('click', (e) => {
+        $('#basic-card').addClass('selected')
+
+        $('#basic-card').animate({
+            left: parseInt($('#basic-card').css('width')) + 48
+        }, 500)
+
+        $('#premium-card').css({'z-index': -1})
+        $('#premium-card').animate({
+            opacity: 0
+        }, 500)
 
         $('#deluxe-card').css({position: 'relative', 'z-index': -1})
         $('#deluxe-card').animate({
@@ -17,5 +34,24 @@ $(document).ready(() => {
         }, 500, () => {
             $('#deluxe-card').css({opacity: 0})
         })
+    })
+
+    $('#deluxe-select').on('click', (e) => {
+        $('#deluxe-card').addClass('selected')
+
+        $('#basic-card').css({position: 'relative', 'z-index': -1})
+        $('#basic-card').animate({
+            left: parseInt($('#basic-card').css('width')) + 48,
+            opacity: 0
+        }, 500)
+
+        $('#premium-card').css({'z-index': -1})
+        $('#premium-card').animate({
+            opacity: 0
+        }, 500)
+
+        $('#deluxe-card').animate({
+            right: parseInt($('#deluxe-card').css('width')) + 48
+        }, 500)
     })
 })
