44563716ad0b91afbe051900dd50e3b7cd75723a
Andy Wang <andynmnwang@gmail.com>
2020-08-13 01:39:17 -0400

    add specific pricing under additional services

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+let finalPrice = 0
+let previous = 0
+const selects = ['#shampoo', '#engine-bay-detail', '#condition']
+const dropdowns = ['#scratch-select', '#stain-select']
+
 $(document).ready(() => {
   $('[data-toggle="tooltip"]').tooltip()
   initializeDrawerPress()
   initializeCalculatorPress()
+  initializeDetailListeners()
+  initializePriceListeners()
 })
 
 function initializeDrawerPress() {
@@ -12,6 +19,7 @@ function initializeDrawerPress() {
 
 function initializeCalculatorPress() {
   $('#calculator').click( () => {
+    $('#final-price').text(finalPrice)
     $('#final-price-container').show(1500)
     $('html, body').animate({
       scrollTop: $("#final-price-container").offset().top
@@ -31,5 +39,42 @@ function countUp() {
         $(this).text(Math.ceil(now));
       }
     })
+  })
+}
+
+function initializeDetailListeners() {
+  $('#scratch-removal').change( () => { $('#scratch-removal').prop('checked') ? $('#scratch-detail').show() : $('#scratch-detail').hide() })
+  $('#stain-removal').change( () => { $('#stain-removal').prop('checked') ? $('#stain-detail').show() : $('#stain-detail').hide() })
+}
+
+function initializePriceListeners() {
+  $('#basic-select').click( () => { finalPrice += 100; })
+  $('#premium-select').click( () => { finalPrice += 130 })
+  $('#deluxe-select').click( () => { finalPrice += 190 })
+
+  selects.forEach( value => {
+    checkSelectListeners(value)
+  })
+
+  dropdowns.forEach( value => {
+    checkDropdownListeners(value)
+  })
+}
+
+function checkSelectListeners(elemId) {
+  $(elemId).change( () => {
+    $(elemId).prop('checked') ? finalPrice += parseInt($(elemId).val()) : finalPrice -= parseInt($(elemId).val())
+  })
+}
+
+function checkDropdownListeners(elemId) {
+  $(elemId).on('focus', function () {
+    previous = parseInt(this.value)
+  }).change( function() {
+    finalPrice -= previous
+    finalPrice += parseInt(this.value)
+    previous = parseInt(this.value)
+
+    console.log(finalPrice)
   })
 }
